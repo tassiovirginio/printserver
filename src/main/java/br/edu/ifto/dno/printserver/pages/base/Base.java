45770d2cd69e009dd834361e726e1e0c92799d8a
Tassio Virginio <tassiovirginio@gmail.com>
2016-09-12 10:23:45 -0300

    melhorando a estrutura do projeto

---
+++ src/main/java/br/edu/ifto/dno/printserver/pages/base/Base.java
@@ -0,0 +1,15 @@
+package br.edu.ifto.dno.printserver.pages.base;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class Base extends WebPage {
+    private static final long serialVersionUID = 1L;
+
+
+    public Base(final PageParameters parameters) {
+        super(parameters);
+
+    }
+
+}
