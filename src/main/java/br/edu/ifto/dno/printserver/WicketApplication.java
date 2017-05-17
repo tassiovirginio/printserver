df3f96f413bed3b18dcefd2c9b58406ef51c246e
Tássio Virgínio <tassiovirginio@gmail.com>
2017-05-17 02:07:49 -0300

    testes com socket

--- src/main/java/br/edu/ifto/dno/printserver/WicketApplication.java
+++ src/main/java/br/edu/ifto/dno/printserver/WicketApplication.java
@@ -1,13 +1,18 @@
 package br.edu.ifto.dno.printserver;
 
+import br.edu.ifto.dno.printserver.business.ImpressoraBusiness;
 import br.edu.ifto.dno.printserver.pages.HomePage;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.response.filter.AjaxServerAndClientTimeFilter;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 import org.apache.wicket.util.file.Folder;
 import org.apache.wicket.util.time.Duration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.logging.Logger;
 
 @Component
@@ -16,6 +21,9 @@ public class WicketApplication extends WebApplication {
     static Logger log = Logger.getLogger(WicketApplication.class.getName());
 
     private Folder uploadFolder = null;
+
+    @Autowired
+    private ImpressoraBusiness impressoraBusiness;
 
     @Override
     public Class<HomePage> getHomePage() {
@@ -26,8 +34,8 @@ public class WicketApplication extends WebApplication {
     public void init() {
 
         log.info("\n" +
-                "********************************************************************\n"+
-                "***                      Carregando o Sistema                    ***\n"+
+                "********************************************************************\n" +
+                "***                      Carregando o Sistema                    ***\n" +
                 "********************************************************************");
 
         getResourceSettings().setResourcePollFrequency(Duration.ONE_SECOND);
@@ -45,7 +53,47 @@ public class WicketApplication extends WebApplication {
         uploadFolder.mkdirs();
         getApplicationSettings().setUploadProgressUpdatesEnabled(true);
 
+        servidor(4631);
+
+
     }
+
+    private void servidor(int porta) {
+        Thread thread = new Thread() {
+            public void run() {
+                System.out.println("Escutando na Porta: " + porta);
+                try {
+                    ServerSocket serverSocket = new ServerSocket(porta);
+                    Socket socket = serverSocket.accept();
+                    InputStream inputStream = socket.getInputStream();
+
+                    BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+                    while (true) {
+                        String cominginText = "";
+                        cominginText = in.readLine();
+                        System.out.println(cominginText);
+                    }
+
+//                    FileOutputStream outputStream =
+//                            new FileOutputStream(new File("teste.pdf"));
+//
+//                    int read = 0;
+//                    byte[] bytes = new byte[1024];
+//
+//                    while ((read = inputStream.read(bytes)) != -1) {
+//                        outputStream.write(bytes, 0, read);
+//                    }
+//                    System.out.println("Arquivo gerado");
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        thread.start();
+    }
+
 
     public Folder getUploadFolder() {
         return uploadFolder;
